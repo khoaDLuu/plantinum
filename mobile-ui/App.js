07dc3ee0fcd7dda4be76008da9b2c71706bdfff7
Helios <hien.dnnt@gmail.com>
2020-03-10 12:53:19 +0700

    create sreen list tree

--- mobile-ui/App.js
+++ mobile-ui/App.js
@@ -1,19 +1,63 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, ActivityIndicator } from 'react-native';
+// import {Font} from 'expo';
+import * as Font from 'expo-font';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Tree from './screens/TreeInformation';
+import Trees from './screens/Trees';
+
+const Stack = createStackNavigator();
+
+export default class App extends React.Component {
+  
+  constructor(){
+    super();
+    this.state={
+      fontLoaded: false
+    }
+  }
+  async componentDidMount() {
+    await Font.loadAsync({
+      Gothic: require('./assets/fonts/SHOWG.ttf')
+    });
+
+    this.setState({ fontLoaded: true });
+  }
+
+  render() {
+
+    return (
+      <NavigationContainer >
+        {this.state.fontLoaded ? (
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={Trees} />
+          <Stack.Screen name="Home2" component={Tree} 
+          options={({route}) => {return {title: route.params.treeName}}} 
+          />
+        </Stack.Navigator>
+        ) : (<ActivityIndicator size="large" style={styles.loading} />
+        )}
+        </NavigationContainer> 
+    
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'stretch',
+    // paddingHorizontal: 16,
+    marginHorizontal: 16
   },
+  loading: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  }
 });
