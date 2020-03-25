dfefd987a2740a9e6083e2a29f92f0e8a32c3e96
khoaDLuu <dangkhoa220899@gmail.com>
2020-03-25 16:35:17 +0700

    do some config

--- server-api/app.py
+++ server-api/app.py
@@ -1,13 +1,17 @@
 import os
 
 from flask import Flask
+from models import db, Plant
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
 
 @app.route('/')
 def hello():
     return {'app': 'plantinum api'}
+
 
 if __name__ == '__main__':
     app.run(debug=True)
