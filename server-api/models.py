f741c4864fdcefc9855cc99ec7a90217c6ad0914
khoaDLuu <dangkhoa220899@gmail.com>
2020-03-25 16:35:54 +0700

    add minor edits to avoid import hell

--- server-api/models.py
+++ server-api/models.py
@@ -1,42 +1,39 @@
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, Date, Float,ForeignKey
+from flask_sqlalchemy import SQLAlchemy
 
-Base = declarative_base()
+db = SQLAlchemy()
 
-class SensorData(Base):
+class SensorData(db.Model):
     __tablename__ = 'sensor_data'
-    id = Column(Integer, primary_key=True)
-    plant_id = Column(Integer, ForeignKey('plant.id'))
-    temp = Column(Float)
-    humidity = Column(Float)
-    moisture = Column(Float)
-    light_intensity = Column(Float)
-    img_url = Column(String)
-    time_recorded = Column(Date)    
-    
+    id = db.Column(db.Integer, primary_key=True)
+    plant_id = db.Column(db.Integer, db.ForeignKey('plant.id'))
+    temp = db.Column(db.Float)
+    humidity = db.Column(db.Float)
+    moisture = db.Column(db.Float)
+    light_intensity = db.Column(db.Float)
+    img_url = db.Column(db.String)
+    time_recorded = db.Column(db.Date)
+
     def __repr__(self):
         return "<SensorData(plant_id='{}', temp='{}', humidity='{}', moisture='{}', light_intensity={},img_url='{}', time_recorded='{}')>"\
                 .format(self.plant_id, self.temp, self.humidity, self.moisture, self.light_intensity, self.img_url, self.time_recorded)
 
-class PlantType(Base):
+class PlantType(db.Model):
     __tablename__ = 'plant_type'
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    water_requirement = Column(Integer)
-   
-    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    water_requirement = db.Column(db.Integer)
+
     def __repr__(self):
         return "<PlantType(name='{}', water_requirement='{}')>"\
                 .format(self.name, self.water_requirement)
 
-class Plant(Base):
+class Plant(db.Model):
     __tablename__ = 'plant'
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    type_id = Column(Integer, ForeignKey('plant_type.id'))
-    date_added = Column(Date) 
-   
-    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    type_id = db.Column(db.Integer, db.ForeignKey('plant_type.id'))
+    date_added = db.Column(db.Date) 
+
     def __repr__(self):
         return "<Plant(name='{}', type_id='{}', date_added='{}')>"\
                 .format(self.name, self.type_id, self.date_added)
