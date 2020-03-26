e189ea7ca3fa954963558ec18c4e453c6ed159a4
khoaDLuu <dangkhoa220899@gmail.com>
2020-03-26 13:28:35 +0700

    Add migration for database

--- server-api/app.py
+++ server-api/app.py
@@ -1,12 +1,14 @@
 import os
 
 from flask import Flask
+from flask_migrate import Migrate
 from models import db, Plant
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
+# migrate = Migrate(app, db)
 
 @app.route('/')
 def hello():
