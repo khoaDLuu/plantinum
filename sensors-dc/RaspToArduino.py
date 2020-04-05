78195ea27a0dd00ec4dbc884a92475cacd769f07
Van Viet <huynhvanviet317@gmail.com>
2020-04-05 21:29:37 +0700

    --- Auto-Git Commit ---

--- sensors-dc/RaspToArduino.py
+++ sensors-dc/RaspToArduino.py
@@ -1,4 +1,4 @@
 import serial  # Khai báo thưu viện serial
 ser = serial.Serial(/dev/ttyUSBx,9600)  # Lưu ý x là số cổng USB 
-While 1:
- ser.readline() #Đọc số từ arduino gửi qua
+while 1:
+ ser.write("1") #gửi số qua arduino thông qua serial
