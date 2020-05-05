e0012eff05bc5f67c482517a9219d5897033d310
khoaDLuu <dangkhoa220899@gmail.com>
2020-05-05 21:18:09 +0700

    refactor recognition_app module

--- sensors-dc/recognition_app.py
+++ sensors-dc/recognition_app.py
@@ -1,13 +1,5 @@
-from keras.preprocessing.image import img_to_array
-from keras.models import load_model
 from picamera.array import PiRGBArray
 from picamera import PiCamera
-from threading import Thread
-import numpy as np
-import imutils
-import time
-import cv2
-import os
 
 from planttp_recognition import guess_planttp
 
@@ -18,7 +10,7 @@ camera = PiCamera()
 rawCapture = PiRGBArray(camera)
 time.sleep(0.1)
 camera.capture(rawCapture, format="bgr")
-image = rawCapture.array
+image = rawCapture.array  # 3-dimensional numpy array
 
 label, proba = guess_planttp(image, MODEL_PATH)
 
