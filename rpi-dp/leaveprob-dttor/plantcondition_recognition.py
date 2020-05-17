f825975b073c2eb07d733881de84a1430b2c961b
viethuynh317 <43821497+viethuynh317@users.noreply.github.com>
2020-05-17 18:22:04 +0700

    fixed plantcondition_recognition done

--- rpi-dp/leaveprob-dttor/plantcondition_recognition.py
+++ rpi-dp/leaveprob-dttor/plantcondition_recognition.py
@@ -9,7 +9,7 @@ import cv2
 import time
 import picamera
 
-def planttp_rec(image, model_path):
+def plantcond_rec(image, model_path):
     IMG_SIZE = 64
     image = cv2.resize(image, (IMG_SIZE, IMG_SIZE))
     image = image.astype("float") / 255.0
