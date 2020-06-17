68dcaacd842829b9e98e5b71041a27fc09a7303a
khoaDLuu <dangkhoa220899@gmail.com>
2020-06-18 01:49:18 +0700

    create main monitoring app

--- sensors-dc/plant_monitor.py
+++ sensors-dc/plant_monitor.py
@@ -1,12 +1,14 @@
+import time
+
 from serial import Serial
 import RPi.GPIO as GPIO
 import requests
 from dotenv import load_dotenv
 
-from plant_recognition import guess_planttp
+from planttp_recognition import guess_planttp
 from cldn_uploading import upload_to_cldn
 from picam import take_photo
-from sensor_read import read_sensor
+# from sensor_read import read_sensor
 from sdata_sending import send_to_server
 from led_control import LedControl
 
@@ -74,6 +76,9 @@ class SerialCom:
     def send_start_sig(self):
         self.ser_to_ardn.write(b'u')
 
+    def send_data_ready_sig(self):
+        self.ser_to_ardn.write(b'1')
+
     def pot_sig(self):
         potd_sig = self.ser_to_ardn.readline()
         if potd_sig == b'Detected\r\n':
@@ -87,6 +92,21 @@ class SerialCom:
                          "neither Detected nor Nothing")
             )
 
+    def read_sensor(self):
+        str_temp = self.ser_to_ardn.readline()
+        temp = float(str_temp)
+    
+        str_humi = self.ser_to_ardn.readline()
+        humi = float(str_humi)
+        
+        str_light = self.ser_to_ardn.readline()
+        light = int(str_light)
+        
+        str_mois = self.ser_to_ardn.readline()
+        mois = int(str_mois)
+
+        return temp, humi, light, mois
+
     def data_ready_sig(self):
         data_ready_sig = self.ser_to_ardn.readline()
         if data_ready_sig == b'Ready\r\n':
@@ -99,25 +119,65 @@ class SerialCom:
 
 
 class PlantMonitor:
-    def __init__(self, serial_com, ind_led):
-        self.ardn_com = serialcom
+    def __init__(self, serial_com, api, ind_led):
+        self.ardn_com = serial_com
+        self.remote_api_com = api
         self.indicator_led = ind_led
         self.plant_existed = False
 
     def start(self):
         self.ardn_com.send_start_sig()
 
-    def run(self):
-        self.start()
-        
+    def shake_hands(self):
+        if self.ardn_com.pot_sig():
+            plant_img = take_photo()
+            label, _ = guess_planttp(plant_img)
+            if label != 'unknown':
+                self.plant_existed = True
+                self.indicator_led.on()
+                self.ardn_com.send_data_ready_sig()
+                return True
+            else:
+                self.plant_existed = False
+                return False
+        else:
+            self.plant_existed = False
+            self.indicator_led.off()
+            return False
+
+    def communicate(self):
         while True:
             try:
-                if self.ardn_com.pot_sig():
-                    pass
-                else:
-                    pass
+                plant_img = take_photo()
+                img_url = upload_to_cldn(plant_img, folder='planttype')
+                sensor_data = self.read_sensor()
 
-            except BadSerialSignal:
+                self.remote_api_com.submit_sensor_data({
+                    "temperature": sensor_data[0],
+                    "humidity": sensor_data[1],
+                    "moisture": sensor_data[2],
+                    "light_intensity": sensor_data[3],
+                    "img_url": image_url
+                })
+                time.sleep(1)
+            
+            except ValueError as e:
+                print(e)
+                pass
+
+            # except ConnectingError:
+            #     pass
+
+    def run(self):
+        self.start()
+
+        while True:
+            try:
+                if self.shake_hands():
+                    self.communicate()
+
+            except BadSerialSignal as e:
+                print(e)
                 pass
             
             except KeyboardInterrupt:
@@ -132,7 +192,7 @@ if __name__ == '__main__':
     indicator_led = LedControl()
     serial_communicator = SerialCom()
     api = APICom('https://plantinum-stage.herokuapp.com', plant_id=10)
-    monitor = PlantMonitor(serial_communicator, indicator_led)
+    monitor = PlantMonitor(serial_communicator, api, indicator_led)
 
     monitor.run()
 
